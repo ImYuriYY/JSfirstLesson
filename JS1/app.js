a98e934a631014f3e316c9e480fc4083e0906e13
ImYuriYY <146898609+ImYuriYY@users.noreply.github.com>
2024-01-24 17:26:32 +0300

    Add files via upload

---
+++ JS1/app.js
@@ -0,0 +1,222 @@
+// РАБОТА С ПЕРЕМЕННЫМИ
+// TASK 1
+
+// const num = 3
+// alert(num)
+
+
+
+
+
+// TASK 2
+
+// const a = 10
+// const b = 2
+
+// alert(`${a + b}, ${a - b}, ${a * b}, ${a / b}`)
+
+
+
+
+
+// TASK 3
+
+// const c = 15
+// const d = 2
+// const result = c + d
+// alert(result)
+
+
+
+
+
+// TASK 4
+
+// const a = 10
+// const b = 2
+// const c = 5
+// alert(a+b+c)
+
+
+
+
+
+// TASK 5
+
+// const a = 17
+// const b = 10
+
+// const c = a - b
+// const d = 7
+
+// const result = c + d
+// alert(result)
+
+
+
+
+
+
+
+
+
+
+// РАБОТА СО СТРОКАМИ
+// TASK 1
+
+// const str = 'Привет, Мир!'
+// alert(str)
+
+
+
+
+
+// TASK 2
+
+// const str1 = 'Привет, '
+// const str2 = 'Мир!'
+// alert(str1 + str2)
+
+
+
+
+
+// TASK 3
+
+// const name = 'Badili'
+// alert(`Hallo, ${name}!`)
+
+
+
+
+
+// TASK 4
+
+// const age = 18
+// alert(`Ich bin ${age} Jahre alt!`)
+
+
+
+
+
+
+
+
+
+
+// ФУНКЦИЯ PROMPT
+// TASK 1
+
+// const userName = prompt('Was heisst du?')
+// alert(`Deine namen ${userName}`)
+
+
+
+
+
+// TASK 2
+
+// const userDigit = prompt('Number?')
+// alert(userDigit**2)
+
+
+
+
+
+
+
+
+
+
+// ОБРАЩЕНИЕ К СИМВОЛАМ СТРОКИ
+// TASK 1
+
+// const str = 'adcde'
+// console.log(str[0])
+// console.log(str[1])
+// console.log(str[2])
+
+
+
+
+
+// TASK 2
+
+// const num = '12345'
+// alert(num[0]*num[1]*num[2]*num[3]*num[4])
+
+
+
+
+
+
+
+
+
+
+// ПРАКТИКА
+// TASK 1
+
+// const hourSeconds = 60 * 60
+// const daySeconds = 24 * hourSeconds
+// const mounthSeconds = 30 * daySeconds
+
+// alert(`Hour: ${hourSeconds}, Day: ${daySeconds}, Mounth: ${mounthSeconds}`)
+
+
+
+
+
+// TASK 2
+
+// const hour = 12
+// const minute = 10
+// const second = 34
+
+// alert(`${hour}:${minute}:${second}`)
+
+
+
+
+
+// TASK 3
+
+// let myNum = 5
+// myNum**=2
+
+// alert(myNum)
+
+
+
+
+
+
+
+
+
+
+// РАБОТА С ПРИСВАИВАНИЕМ И ДЕКРЕМЕНТАМИ
+// TASK 1
+
+// let num = 47;
+// num += 7
+// num -= 18
+// num *= 10
+// num /= 15
+// alert(num);
+
+
+
+
+
+// TASK 2
+
+// let num = 10
+// num++
+// num++
+// num--
+// alert(num)
+
+
+
+
